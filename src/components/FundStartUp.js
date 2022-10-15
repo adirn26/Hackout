44a0e4024fb654e7af7edd8371bcfd1587e418ea
anusha-shanbhog <anushapshanbhog@gmail.com>
2022-10-15 15:36:01 +0530

    frontend basic added

---
+++ src/components/FundStartUp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FundStartUp() {
+  return (
+    <div>FundStartUp</div>
+  )
+}
+
+export default FundStartUp
---
+++ src/components/GetFunding.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GetFunding() {
+  return (
+    <div></div>
+  )
+}
+
+export default GetFunding
